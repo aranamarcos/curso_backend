4df9dc985d69d7ed837c44db02f89bcd501817cd
aranamarcos <arana.marcos@gmail.com>
2023-09-15 00:47:29 -0300

    2da Entrega

--- 06-Entrega_2/src/index.js
+++ 06-Entrega_2/src/index.js
@@ -5,8 +5,10 @@ import { Server } from 'socket.io';
 import { __dirname } from './path.js';
 import mongoose from "mongoose";
 
+import path from 'path'
 import productRouter from './routes/products.routes.js'
 import cartRouter from './routes/cart.routes.js'
+import productModel from './models/products.models.js';
 
 
 const app = express();
@@ -60,10 +62,6 @@ io.on('connection', (socket) => {
 app.use(express.json());
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
-
-app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`)
-})
 
 
 // Handlebars
